c393afbd1ecfeeb0f8e3cc712aba322804fa92f6
TrangAlexis <trang.alexis@hotmail.fr>
2021-12-30 04:55:32 +0100

    Redefined class Domain, and added script for step 2
    Class Domain is now more precise (can us .PDB to know the PDB code of a domain, .chain to know it's chain, and .location to get the range of the domain)
    
    The new script (Project_step_2.py) will add to the Domains their pfam accession code. It's really slow, but I'm pretty sure it works.

--- src/Project_step_1.py
+++ src/Project_step_1.py
@@ -1,20 +1,29 @@
 import os
 from time import time
-
+import re
 os.chdir("C:/Users/Alexis Trang/Documents/Cours_UPMC_M2/GENOM")
 
+
+#####EXEMPLE
+phrase="123 A:45-67"
+phrase1="116748\tsp\ta.1.1.1\t-\tBacillus subtilis [TaxId: 1423]\n"
+#############
 class Domain:
-    def __init__(self,ID,SCOPe,domain,chain):
+    def __init__(self,ID,SCOPe,domain,PDB=None,chain=None,location=None,Pfam_accession=None):
         self.ID=ID
         self.SCOPe=SCOPe
         self.domain=domain
+        self.PDB=PDB
         self.chain=chain
-
+        self.location=location
+        self.Pfam_accession=Pfam_accession
 ############################
 
-soluble_domains=[]
+soluble_domains=[]  #List of domains
+PDBs=[]   #List of PDB codes of soluble domains
 
 scope_file=open("dir.des.scope.2.08-stable.txt",'r')
+
 for i in range(5):
     line=scope_file.readline()
 
@@ -23,10 +32,16 @@ while(line!=""):
     line=line.split("\t")
     line[-1]=line[-1][:-1]
     if line[2][0] in ['a','b','c','d']:
-        soluble_domains.append(Domain(line[0],line[2],line[3],line[4]))
+        if re.findall(r" [A-Z]:",line[4])==[]:
+            soluble_domains.append(Domain(line[0],line[2],line[3],line[4]))
+            PDBs.append(line[4])
+        else:
+            PDB=re.findall(r".+ ",line[4])[0][:-1]
+            chain=re.findall(r"[A-Z]:",line[4])[0][0]
+            location=re.findall(r"[0-9]+-[0-9]+",line[4])
+            soluble_domains.append(Domain(line[0],line[2],line[3],PDB,chain,location))
+            PDBs.append(PDB)
     line=scope_file.readline()
-
-
 
 scope_file.close()
 
---
+++ src/Project_step_2.py
@@ -0,0 +1,65 @@
+import os
+from time import time
+
+#PLEASE RUN Project_step_1.py before running that
+os.chdir("C:/Users/Alexis Trang/Documents/Cours_UPMC_M2/GENOM")
+pfam_mapping=open("pdb_pfam_mapping.txt",'r')
+t1=time()
+Pfam_accessions=[]
+
+for i in range(2):
+    line=pfam_mapping.readline()
+
+columns=line.split()
+
+line=pfam_mapping.readline()
+
+
+while line!="":
+    line=line.split()
+    if line[0] in PDBs:
+        indices_PDBs=[ind for ind in range(len(PDBs)) if PDBs[ind]==line[0]]
+        #print(indices_PDBs)
+        #print("###################\n")
+        for i in indices_PDBs:
+            #print("sol:",soluble_domains[i].chain,"pfam:",line[1])
+
+            if soluble_domains[i].chain==line[1]:
+                #print("MATCH")
+                soluble_domains[i].Pfam_accession=line[4]
+                PDBs[i]=-1
+            #print(soluble_domains[i].PDB)
+        #print("###################\n*\n*\n*\n*\n*\n*")
+
+        #print(line[0],line[4])
+    line=pfam_mapping.readline()
+
+
+pfam_mapping.close()
+print(time()-t1)
+
+
+############
+"""
+while line!="":
+    line=line.split()
+    if line[0] in PDBs:
+        indices_PDBs=[ind for ind in range(len(PDBs)) if PDBs[ind]==line[0]]
+        #print(indices_PDBs)
+        #print("###################\n")
+        for i in indices_PDBs:
+            #print("sol:",soluble_domains[i].chain,"pfam:",line[1])
+
+            if soluble_domains[i].chain==line[1]:
+                #print("MATCH")
+                print("sol:",soluble_domains[i].location,"pfam:","[\'"+line[2]+"-"+line[3]+"\']")
+                if soluble_domains[i].location==line[2]+"-"+line[3]:
+                    print("YUP")
+                    soluble_domains[i].Pfam_accession=line[4]
+                    PDBs[i]=-1
+            #print(soluble_domains[i].PDB)
+        #print("###################\n*\n*\n*\n*\n*\n*")
+
+        #print(line[0],line[4])
+    line=pfam_mapping.readline()
+"""
