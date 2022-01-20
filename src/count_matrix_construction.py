2100e6bd51156aab41c1fcd1c123493ceb30edf8
LiamLeGoffic <liamlegoffic@yahoo.fr>
2022-01-20 14:41:45 +0100

    Update count_matrix_construction.py

--- src/count_matrix_construction.py
+++ src/count_matrix_construction.py
@@ -1,14 +1,10 @@
-import pandas as pd
 import numpy as np
 import json
-import os
-import matplotlib.pyplot as plt
 from itertools import combinations
-from IPython.display import clear_output
-from math import *
 from src.find_HC import *
 
-# Binarizate a sequene specifically to have 1 at HC emplacements
+###############################################################################################################################
+# Binarize a sequene specifically to have 1 at HC emplacements
 # input: string sequence and dictionary containing positions of hcs on each sequence
 # output: binary sequence
 def light_HCs(seq, hcs):
@@ -18,13 +14,19 @@ def light_HCs(seq, hcs):
             seq[aa]=1
     return seq
 
-def get_hc_from_dico(alignment):
+# Get all binarized sequences from a dictionary, especially those in repeted sequences
+# input: dictionary associating sequence to a list containing the binarized sequence a number of times equal to the number of occurences
+# output: generator of binarized sequences
+def get_binarized_sequences(alignment):
     for HCs in alignment.values():
         for hcs in HCs:
             yield hcs
 
+# Find conserved regions of an binarized sequences alignment (rate of HCs appearance on a position above a threshold)
+# input: dictionary associating sequence to a list containing the binarized sequence a number of times equal to the number of occurences and a threshold between 0 and 1
+# output: list of lists containing regions positions
 def find_conserved_regions(alignment, threshold):
-    alignment_hc=list(get_hc_from_dico(alignment))
+    alignment_hc=list(get_binarized_sequences(alignment))
     coverages = np.mean(list(alignment_hc), axis=0)
     n = len(coverages)
     conserved_pos = []
@@ -39,21 +41,34 @@ def find_conserved_regions(alignment, threshold):
         conserved_pos.append(pos)
     return conserved_pos
 
+###############################################################################################################################
+# Find HCs corresponding to the positions of a conserved region 
+# input: list of positions and dictionary containing HCs positions associated to the HC
+# output: matching HC generator
 def find_region(pos, HCs):
     for pos_hc in HCs:
         if (pos[0]>=pos_hc[0] and pos[0]<=pos_hc[1]) or (pos[-1]>=pos_hc[0] and pos[-1]<=pos_hc[1]):
             yield HCs[pos_hc]
 
+# Find all HCs corresponding with conserved regions
+# input: list of list containing all conserved positions, sequence of the alignment and dictionary associating a sequence with its HCs positions and HCs patterns
+# output: generator of HCs matching with a conserved region
 def find_corresponding_HCs(conserved_pos, seq, HCs):
     for pos in conserved_pos:
         yield list(find_region(pos, HCs[seq][0]))
 
+# Get a the count of appearance of each HC in an list
+# input: list of HCs
+# output: dictionary of each HC associated with its number of appearances
 def count_hc(substitutes):
     dict_substitutes = dict([(sub, 0) for sub in set(substitutes)])
     for sub in substitutes:
         dict_substitutes[sub]+=1
     return dict_substitutes
 
+# Add the substitution counts to the matrix
+# input: numpy matrix and list of HCs
+# output: numpy matrix
 def add_count(matrix, substitutes):
     substitutes = count_hc(substitutes)
     for sub,count in substitutes.items():
@@ -62,6 +77,9 @@ def add_count(matrix, substitutes):
         matrix[combi[0], combi[1]]+=substitutes[combi[0]]+substitutes[combi[1]]
     return matrix
 
+# Update the matrix with the subsitution counts of an alignment
+# input: numpy matrix, list of lists containing positions of a conserved position and list of the most common HCs
+# output: numpy matrix
 def update_matrix(matrix, conserved_pos, HCs, common_HCs):
     order = list(common_HCs.keys())
     n=len(conserved_pos)
@@ -80,12 +98,20 @@ def update_matrix(matrix, conserved_pos, HCs, common_HCs):
         matrix = add_count(matrix, conserved_hcs[i])
     return matrix
 
-def symetrize(mat):
+###############################################################################################################################
+# Get a symmetric matrix
+# input: triangular matrix
+# output: symmetric matrix
+def symmetrize(mat):
     for i in range(len(mat)-1):
         for j in range(i+1, len(mat)):
             mat[j, i]=mat[i, j]
     return mat
 
+###############################################################################################################################
+# Get the count matrix containing all the substitutions that appears in the different alignments on conserved regions
+# input: path to file containing all the alignment, path to file containing all the soluble domains, path to file containing the result of an HC count (obtained by the function "get_analyse()" for example), the coverage threshold, path to the output file, boolean to have a pseudo-count or no
+# output: file containing the matrix and number of conserved regions in all the alignments
 def get_count_matrix(alignments_file, PF_file, HC_counts_file, threshold, output_file, pseudo_count=False):
     print('Preprocessing...')
     data = read_data(alignments_file, PF_file)
@@ -97,13 +123,13 @@ def get_count_matrix(alignments_file, PF_file, HC_counts_file, threshold, output
     nb_alignments = len(data)
     counter = 0
     n = len(common_HCs)
-    HCs_conserved=0
+    conserved_regions=0
     if pseudo_count:
         matrix = np.ones((n, n))
     else:
         matrix = np.zeros((n, n))
     for alignment in data.values():
-        counter+=1
+        counter += 1
         print(counter, '/', nb_alignments)
         print(len(alignment), 'x', len(alignment[0]))
         HCs_alignment = {}
@@ -117,10 +143,10 @@ def get_count_matrix(alignments_file, PF_file, HC_counts_file, threshold, output
                 HCs_alignment[seq].append(light_HCs(seq, HCs_seq))
                 alignment_HCs[seq].append(HCs_seq)
         conserved_pos = find_conserved_regions(HCs_alignment, threshold)
-        HCs_conserved+=len(conserved_pos)
+        conserved_regions += len(conserved_pos)
         matrix = update_matrix(matrix, conserved_pos, alignment_HCs, common_HCs)
-    matrix = symetrize(matrix)
+    matrix = symmetrize(matrix)
     print('done')
     # save results
     np.savetxt(output_file, matrix, delimiter=' ', fmt='%d')
-    return len(data), HCs_conserved
+    return conserved_regions
